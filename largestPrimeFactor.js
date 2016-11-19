c9e3f15a1a7509475bc162917a9586fb86b93aa7
Andrea <takamiya.andrea@gmail.com>
2016-11-19 10:03:37 -1000

    Getting numbers divisible by n, 5,7,13,29 are the first 4 of the array

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,39 @@
 exports.largestPrimeFactor = function(n){
+
   var primeNumber = 0;
   // do your work here
 
+
+  //prime = divides by n without a remainder
+  //
+  //n > 1
+  //is prime if it CANNOT be written as a product of a * b
+  //n can only be divided by 1 and itself
+
+var narrowedDown = [];
+
+
+for (var i = 2; i < n; i++) {
+  //i will be any number between 2 and n
+  //narrow down i until you get prime factors of n
+
+  if (n % i === 0){
+    //if n is divisible by 0, then store i
+
+    narrowedDown.push(i);
+  }
+
+
+}
+
+
+console.log(narrowedDown);
+
+
+// find square root of the
+
+
   return primeNumber;
 };
+
+
